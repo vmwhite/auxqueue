d797db61c0ecda3f0865e2c2e770259356d39f50
Veronica M White <vronniewhite@gmail.com>
2023-09-26 16:54:56 -0500

    added A test ADDING SECOND b TEST

--- tests/testthat/test-Calc_Am.R
+++ tests/testthat/test-Calc_Am.R
@@ -18,6 +18,7 @@ test_that("check A_k matrix", {
                      0, 0,0,lam,  0,
                      0, 0,0,0,lam)
                    , nrow = 5, ncol = 5, byrow = TRUE)
+  ## Error in Green text transition from (i, 2) to (i,1) should be mu_A not mu_A*p
   A_1 <-    matrix(c(-(lam+3*mu_P),0,0,0,0,
                      mu_A*p,-(lam+calc_mu_mn(2,1,mu_P,mu_A)),0,0, 0,
                      0, mu_A,-(lam+calc_mu_mn(2,1,mu_P,mu_A)),0,  0,
@@ -30,10 +31,11 @@ test_that("check A_k matrix", {
                      0, 0,0,2*mu_P*p,  0,
                      0, 0,0,0,2*mu_P)
                    , nrow = 5, ncol = 5, byrow = TRUE)
+  ## Error in Green text transition from (i, 3) to (i-2,4) should be 2*mu_P*q not 2*mu_P*calc_alpha_i(p,1)
   A_3 <-    matrix(c(0,3*mu_P*q,0,0,0,
                      0, 0,2*mu_P*calc_alpha_i(p,1),0, 0,
                      0, 0,0,2*mu_P*calc_alpha_i(p,1),  0,
-                     0, 0,0,0, 2*mu_P*q, ## check if this should be q or calc_alpha_i(p,1)
+                     0, 0,0,0, 2*mu_P*q,
                      0,0,0,0, 0)
                    , nrow = 5, ncol = 5, byrow = TRUE)
 
@@ -58,4 +60,35 @@ test_that("check A_k matrix", {
   expect_equal(A[4,,], A_3)
   expect_equal(A[5,,], A_4)
   expect_equal(A[6,,], A_5)
+
+
+
+  ##### hard coded matrices smaller case test
+  K <- 2
+  r <- 2
+  s <- 1
+  A_0 <-    matrix(c(lam,0,0,
+                     0,lam,0,
+                     0, 0,lam)
+                   , nrow = 3, ncol = 3, byrow = TRUE)
+  A_1 <-    matrix(c(-(lam+2*mu_P),0,0,
+                     mu_A*p,-(lam+mu_P +mu_A),  0,
+                     0,mu_A,-(lam+mu_P+s*mu_A))
+                   , nrow = 3, ncol = 3, byrow = TRUE)
+  A_2 <-    matrix(c(2*mu_P*p,0,0,
+                     0, mu_P*p+mu_A*q,  0,
+                     0,0,mu_P)
+                   , nrow = 3, ncol = 3, byrow = TRUE)
+  A_3 <-    matrix(c(0,2*mu_P*q,0,
+                     0, 0,mu_P*q,
+                     0,0,0)
+                   , nrow = 3, ncol = 3, byrow = TRUE)
+
+  #### calc Am
+  A <- Calc_Am(K,s,r,lam,lam_P,lam_A,mu_P,mu_A, p)
+  expect_equal(A[1,,], A_0)
+  expect_equal(A[2,,], A_1)
+  expect_equal(A[3,,], A_2)
+  expect_equal(A[4,,], A_3)
+
 })
--- tests/testthat/test-Calc_B_mn.R
+++ tests/testthat/test-Calc_B_mn.R
@@ -37,7 +37,8 @@ test_that("Test B matrix calculations", {
                   0,0, mu_A,-(lam +calc_mu_mn(1,1, mu_P,mu_A)),  lam_A,
                   0,0,0,mu_A,-(lam +calc_mu_mn(1,1, mu_P,mu_A)))
                 , nrow = 5, ncol = 5, byrow = TRUE)
-  B_12 = matrix(c(-(lam+(2*mu_P)),lam_A,0, 0, 0,
+  ## Error in Green Text, transition from (2,2+x) to (2,3+x) where 0<=x<=K-3 should be equal to 0 not lam_A
+   B_12 = matrix(c(-(lam+(2*mu_P)),lam_A,0, 0, 0,
                   mu_A,-(lam +calc_mu_mn(2,1, mu_P,mu_A)),0,  0, 0,
                   0,mu_A,-(lam +calc_mu_mn(2,1, mu_P,mu_A)),  0, 0,
                   0,0, mu_A,-(lam +calc_mu_mn(2,1, mu_P,mu_A)),  0,
@@ -109,4 +110,62 @@ test_that("Test B matrix calculations", {
   expect_equal(B[5,6,,], B_45)
   expect_equal(B[6,6,,], B_55)
   expect_equal(B[6,7,,], B_56)
+
+  ##### hard coded matrices smaller case test
+  K <- 2
+  r <- 2
+  s <- 1
+
+  B_00 = matrix(c(lam_P,0,0,
+                  0,lam_P,0,
+                  0,0,lam)
+                , nrow = 3, ncol = 3, byrow = TRUE)
+  B_01 = matrix(c(lam_P,0,0, 0, 0,
+                  0,lam_P,0,  0, 0,
+                  0,0,lam_P,  0, 0,
+                  0,0,0,lam_P, 0,
+                  0,0,0,0,lam)
+                , nrow = 3, ncol = 3, byrow = TRUE)
+  B_10 = matrix(c(-(lam+(0*mu_P)),lam_A,0, 0, 0,
+                  mu_A,-(lam +calc_mu_mn(0,1, mu_P,mu_A)),lam_A,  0, 0,
+                  0,mu_A,-(lam +calc_mu_mn(0,1, mu_P,mu_A)),  lam_A, 0,
+                  0,0, mu_A,-(lam +calc_mu_mn(0,1, mu_P,mu_A)),  lam_A,
+                  0,0,0,mu_A,-(lam +calc_mu_mn(0,1, mu_P,mu_A)))
+                , nrow = 3, ncol = 3, byrow = TRUE)
+  B_11 = matrix(c(-(lam+(1*mu_P)),lam_A,0, 0, 0,
+                  mu_A,-(lam +calc_mu_mn(1,1, mu_P,mu_A)),lam_A,  0, 0,
+                  0,mu_A,-(lam +calc_mu_mn(1,1, mu_P,mu_A)),  lam_A, 0,
+                  0,0, mu_A,-(lam +calc_mu_mn(1,1, mu_P,mu_A)),  lam_A,
+                  0,0,0,mu_A,-(lam +calc_mu_mn(1,1, mu_P,mu_A)))
+                , nrow = 3, ncol = 3, byrow = TRUE)
+  B_21 = matrix(c(mu_P,0,0, 0, 0,
+                  0,mu_P,0,  0, 0,
+                  0,0,mu_P,  0, 0,
+                  0,0,0,mu_P, 0,
+                  0,0,0,0,mu_P)
+                , nrow = 3, ncol = 3, byrow = TRUE)
+  B_22 = matrix(c(2*mu_P,0,0, 0, 0,
+                  0,2*mu_P,0,  0, 0,
+                  0,0,2*mu_P,  0, 0,
+                  0,0,0,2*mu_P, 0,
+                  0,0,0,0, 2*mu_P)
+                , nrow = 3, ncol = 3, byrow = TRUE)
+  B_32 = matrix(c(0,3*mu_P*q,0,0, 0,
+                  0,0,2*mu_P*calc_alpha_i(p,1),0, 0,
+                  0,0,0, 2*mu_P*calc_alpha_i(p,1), 0,
+                  0,0,0,  0, 2*mu_P*q,
+                  0,0,0,0, 0)
+                , nrow = 3, ncol = 3, byrow = TRUE)
+
+  B <- Calc_Bmn(K,s,r,lam,lam_A,lam_P,mu_P,mu_A, p)
+
+  #Check values -- Note: R indexes at 1
+  expect_equal(B[1,1,,], B_00)
+  expect_equal(B[1,2,,], B_01)
+  expect_equal(B[2,1,,], B_10)
+  expect_equal(B[2,2,,], B_11)
+  expect_equal(B[3,2,,], B_21)
+  expect_equal(B[3,3,,], B_22)
+  expect_equal(B[4,3,,], B_32)
+
 })
